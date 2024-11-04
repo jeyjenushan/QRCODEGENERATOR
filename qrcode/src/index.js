235892a1e440e97e826952f66efe156414aff4b2
Jeyaruban jenushan <jjenushan550@gmail.com>
2024-11-04 21:54:04 +0530

    change

---
+++ qrcode/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import QrCode from './QrCode';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+<QrCode/>
+  </React.StrictMode>
+);
+
+
